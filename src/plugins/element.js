524ea7fd8fe8467863ada19bc9413c6d18707bdb
Sql13525482154 <duex0702@gmail.com>
2020-12-28 11:32:41 +0800

    登录功能完成

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button,Form,FormItem,Input,Icon,Message} from 'element-ui'
 
-Vue.use(Button)
+Vue.use(Button);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
+Vue.use(Icon);
+// 挂载到原型对象 可以用this访问$message进行弹框提示
+Vue.prototype.$message = Message
